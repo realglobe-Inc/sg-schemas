5c3e65c6560b47a01e651030d7eb9fa43580147e
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-07 18:53:44 +0900

    Version incremented to 1.0.1

---
+++ test/index_test.js
@@ -0,0 +1,17 @@
+/**
+ * Test for index js
+ */
+'use strict'
+
+const sgSchemas = require('../lib')
+const assert = require('assert')
+
+describe('index', function () {
+  it('Eval props', () => {
+    for (let name of Object.keys(sgSchemas)) {
+      assert.ok(sgSchemas[ name ])
+    }
+  })
+})
+
+/* global describe, it*/
