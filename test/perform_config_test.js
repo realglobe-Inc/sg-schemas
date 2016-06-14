8c4e423df83d6a9aea6334e1f781c747207a4d3d
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-14 17:26:50 +0900

    Modify interface

--- test/perform_config_test.js
+++ test/perform_config_test.js
@@ -6,6 +6,7 @@
 
 const performConfig = require('../lib/perform_config.json')
 const assert = require('assert')
+const tv4 = require('tv4')
 const co = require('co')
 
 describe('perform-config', () => {
@@ -18,7 +19,23 @@ describe('perform-config', () => {
   }))
 
   it('Perform config', () => co(function * () {
+    it('Validate example', () => co(function * () {
+      let result = tv4.validateResult(
+        require('../example/example-perform-config.json'),
+        performConfig
+      )
+      assert.ok(result.valid)
+    }))
 
+    it('Validate invalid schema', () => co(function * () {
+      let result = tv4.validateResult(
+        {
+          interface: 'hoge'
+        },
+        performConfig
+      )
+      assert.ok(!result.valid)
+    }))
   }))
 })
 
